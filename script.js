ff71d92797f0f7307f9eec1c9bea738accac4d90
Soulaymane01 <soulaynadi@gmail.com>
2024-11-14 22:10:42 +0100

    Updated the CTA link

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Open website to purches the proramme
 document.querySelectorAll('.cta-button').forEach(button => {
     button.addEventListener('click', () => {
-        window.location.href = 'https://chaimaetouhami.com/products/consultations';
+        window.location.href = 'https://chaimaetouhami.kit.com/products/consultations';
     });
 });
